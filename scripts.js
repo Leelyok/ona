502d18609169092a1ef60706c1070582d55f6c04
Lily <89401279+Leelyok@users.noreply.github.com>
2022-03-22 12:45:02 +0300

    add acordions :))

--- scripts.js
+++ scripts.js
@@ -52,6 +52,29 @@ $(document).ready(function() {
 
 });
 
+// ACORDONS
+
+$.fn.accordion = function() {
+  const trigger = $(this).find('.accordion-trigger');
+  const collapse = $(this).find('.accordion-collapse');
+
+  $(trigger).first().addClass('accordion-open');
+  $(collapse).first().show();
+
+  $(trigger).each(function(){
+    $(this).on('click', function(){
+      $(this).siblings('.accordion-collapse').slideToggle();
+      $(this).toggleClass('accordion-open');
+      $(this).parent().siblings('.accordion-item').find('.accordion-trigger').removeClass('accordion-open');
+      $(this).parent().siblings('.accordion-item').find('.accordion-collapse').slideUp();
+    });
+  });
+}
+
+$('.accordion').accordion();
+$('.accordion2').accordion();
+
+
 $('.slider-container').slick({
   // Setting name: setting-value
 });
